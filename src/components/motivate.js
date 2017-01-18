00414f66a0c98b87610e53c61c1501a877323b5b
Janie <jtito@hotmail.com>
2017-01-19 09:53:45 +1300

    more content and styling to motivate page

--- src/components/motivate.js
+++ src/components/motivate.js
@@ -1,4 +1,5 @@
 const React = require('react')
+import RaisedButton from 'material-ui/RaisedButton'
 
 module.exports = function (props) {
   const { route, appName, topic } = props
@@ -7,7 +8,28 @@ module.exports = function (props) {
     <div>
       <h1>āwhina</h1>
       <h4>Motivation and Inspiration</h4>
-      
+
+      <div className ='item'>
+        media item
+      </div>
+
+      <div className ='source'>
+        author or source
+      </div>
+      <br/>
+
+      <div className="newTopic">
+        <button>Click for another one</button>
+      </div>
+
+      <div id="mediaChoice">
+        <p>Select the type of media</p>
+        <input type="checkbox" name="choose" value="text" default="true" checked/> Text
+        <input type="checkbox" name="choose" value="image" checked/> Image
+        <input type="checkbox" name="choose" value="video" checked/> Video
+      </div>
+
     </div>
-)
+  )
+
 }
